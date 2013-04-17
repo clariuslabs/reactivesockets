82dc6e68afe92d928f0e197a847461a2f7d51444
Daniel Cazzulino <daniel@cazzulino.com>
2013-04-17 16:07:35 -0300

    Avoid waiting for folder deletions on build. Added separate dirs for net40 and net45

--- build.bat
+++ build.bat
@@ -1,33 +1,26 @@
 echo Initializing
-rmdir /s /q out
+rmdir /s /q out45
+rmdir /s /q out40
 .nuget\nuget install .\ReactiveSockets\packages.config -OutputDirectory packages
 
 echo Building stable version
 
-md .\out
-md .\out\lib
-md .\out\lib\net45
+md .\out45
+md .\out45\lib
+md .\out45\lib\net45
 msbuild .\ReactiveSockets\ReactiveSockets.csproj /p:Configuration=Release /p:TargetFrameworkVersion=v4.5 /t:Rebuild
-xcopy .\ReactiveSockets\bin\Release\ReactiveSockets.dll .\out\lib\net45\* /Y
-xcopy .\ReactiveSockets\bin\Release\ReactiveSockets.xml .\out\lib\net45\* /Y
-xcopy .\ReactiveSockets.nuspec out\* /Y
-.nuget\nuget pack .\out\ReactiveSockets.nuspec
-
-timeout 1
-rmdir /s /q out
-timeout 1
-
+xcopy .\ReactiveSockets\bin\Release\ReactiveSockets.dll .\out45\lib\net45\* /Y
+xcopy .\ReactiveSockets\bin\Release\ReactiveSockets.xml .\out45\lib\net45\* /Y
+xcopy .\ReactiveSockets.nuspec .\out45\* /Y
+.nuget\nuget pack .\out45\ReactiveSockets.nuspec
 
 echo Building prerelease version
 
-md .\out
-md .\out\lib
-md .\out\lib\net40
+md .\out40
+md .\out40\lib
+md .\out40\lib\net40
 msbuild .\ReactiveSockets\ReactiveSockets.csproj /p:Configuration=Release /p:TargetFrameworkVersion=v4.0 /t:Rebuild
-xcopy .\ReactiveSockets\bin\Release\ReactiveSockets.dll .\out\lib\net40\* /Y
-xcopy .\ReactiveSockets\bin\Release\ReactiveSockets.xml .\out\lib\net40\* /Y
-xcopy .\ReactiveSockets-rc.nuspec .\out\* /Y
-.nuget\nuget pack .\out\ReactiveSockets-rc.nuspec
-
-timeout 1
-rmdir /s /q out
+xcopy .\ReactiveSockets\bin\Release\ReactiveSockets.dll .\out40\lib\net40\* /Y
+xcopy .\ReactiveSockets\bin\Release\ReactiveSockets.xml .\out40\lib\net40\* /Y
+xcopy .\ReactiveSockets-rc.nuspec .\out40\* /Y
+.nuget\nuget pack .\out40\ReactiveSockets-rc.nuspec
