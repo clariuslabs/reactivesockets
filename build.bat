423678a9f0f2f5f9173b3d709ba3c08e3567b469
Daniel Cazzulino <daniel@cazzulino.com>
2013-04-17 13:01:36 -0300

    Fixed reference to platform services.

--- build.bat
+++ build.bat
@@ -1,23 +1,33 @@
 echo Initializing
 rmdir /s /q out
-.nuget\nuget install ReactiveSockets\packages.config -OutputDirectory packages
+.nuget\nuget install .\ReactiveSockets\packages.config -OutputDirectory packages
 
 echo Building stable version
 
-md out\lib\net45
-msbuild ReactiveSockets\ReactiveSockets.csproj /p:Configuration=Release /p:TargetFrameworkVersion=v4.5 /t:Rebuild
-copy ReactiveSockets\bin\Release\ReactiveSockets.dll out\lib\net45\ReactiveSockets.dll
-copy ReactiveSockets\bin\Release\ReactiveSockets.xml out\lib\net45\ReactiveSockets.xml
-copy ReactiveSockets.nuspec out\ReactiveSockets.nuspec
-.nuget\nuget pack out\ReactiveSockets.nuspec
+md .\out
+md .\out\lib
+md .\out\lib\net45
+msbuild .\ReactiveSockets\ReactiveSockets.csproj /p:Configuration=Release /p:TargetFrameworkVersion=v4.5 /t:Rebuild
+xcopy .\ReactiveSockets\bin\Release\ReactiveSockets.dll .\out\lib\net45\* /Y
+xcopy .\ReactiveSockets\bin\Release\ReactiveSockets.xml .\out\lib\net45\* /Y
+xcopy .\ReactiveSockets.nuspec out\* /Y
+.nuget\nuget pack .\out\ReactiveSockets.nuspec
+
+timeout 1
 rmdir /s /q out
+timeout 1
+
 
 echo Building prerelease version
 
-md out\lib\net40
-msbuild ReactiveSockets\ReactiveSockets.csproj /p:Configuration=Release /p:TargetFrameworkVersion=v4.0 /t:Rebuild
-copy ReactiveSockets\bin\Release\ReactiveSockets.dll out\lib\net40\ReactiveSockets.dll
-copy ReactiveSockets\bin\Release\ReactiveSockets.xml out\lib\net40\ReactiveSockets.xml
-copy ReactiveSockets-rc.nuspec out\ReactiveSockets.nuspec
-.nuget\nuget pack out\ReactiveSockets.nuspec
+md .\out
+md .\out\lib
+md .\out\lib\net40
+msbuild .\ReactiveSockets\ReactiveSockets.csproj /p:Configuration=Release /p:TargetFrameworkVersion=v4.0 /t:Rebuild
+xcopy .\ReactiveSockets\bin\Release\ReactiveSockets.dll .\out\lib\net40\* /Y
+xcopy .\ReactiveSockets\bin\Release\ReactiveSockets.xml .\out\lib\net40\* /Y
+xcopy .\ReactiveSockets-rc.nuspec .\out\* /Y
+.nuget\nuget pack .\out\ReactiveSockets-rc.nuspec
+
+timeout 1
 rmdir /s /q out
