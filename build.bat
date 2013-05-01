8a460eab0972bebc79f56d3d5ef5ff24720a015b
Greg MacLellan <github@list.mtechsolutions.ca>
2013-05-01 15:48:36 -0300

    Find MSBuild v4 in path
    I suspect this may have been the cause of #1

--- build.bat
+++ build.bat
@@ -2,10 +2,12 @@ echo Initializing
 rmdir /s /q out40
 .nuget\nuget install .\ReactiveSockets\packages.config -OutputDirectory packages
 
+for /D %%D in (%SYSTEMROOT%\Microsoft.NET\Framework\v4*) do set msbuild=%%D\MSBuild.exe
+
 md .\out40
 md .\out40\lib
 md .\out40\lib\net40
-msbuild .\ReactiveSockets\ReactiveSockets.csproj /p:Configuration=Release /p:TargetFrameworkVersion=v4.0 /t:Rebuild
+%msbuild% .\ReactiveSockets\ReactiveSockets.csproj /p:Configuration=Release /p:TargetFrameworkVersion=v4.0 /t:Rebuild
 xcopy .\ReactiveSockets\bin\Release\ReactiveSockets.dll .\out40\lib\net40\* /Y
 xcopy .\ReactiveSockets\bin\Release\ReactiveSockets.xml .\out40\lib\net40\* /Y
 xcopy .\ReactiveSockets.nuspec .\out40\* /Y
