3565b71c23bb28e5e55bc61eb2ff62d6af46c4f6
Dennis Daume <daume.dennis@gmail.com>
2013-12-05 22:28:32 +0100

    Complete the listener observable when disposing

---
+++ ReactiveSockets.Tests/ReactiveListenerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ReactiveSockets.Tests
+{
+    public class ReactiveListenerTests
+    {
+        [Fact]
+        public void when_disposed_then_complete_connections_observable()
+        {
+            var listener = new ReactiveListener(1055);
+            listener.Start();
+
+            bool completed = false;
+
+            listener.Connections.Subscribe(x => { }, () => completed = true);
+
+            listener.Dispose();
+
+            Assert.True(completed);
+        }
+    }
+}
